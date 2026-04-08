047b970112f1d5d74d9266c026373eb02d2b9e69
Ryan364412 <bundhoor50@gmail.com>
2026-04-08 10:04:20 +0400

    hb

--- part2/app/config.py
+++ part2/app/config.py
@@ -1,18 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import config
 
-db = SQLAlchemy()
+import os
 
-def create_app(config_class=config['development']):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
 
-    # Initialize SQLAlchemy
-    db.init_app(app)
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Register blueprints here if needed
-    # from app.routes import main
-    # app.register_blueprint(main)
-
-    return app
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
